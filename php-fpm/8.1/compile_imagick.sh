26d755ab60c0240cf04412d33df3df7f473a412d
Florian <florian.schmidt.welzow@t-online.de>
2021-12-16 21:19:58 +0100

    Fix build of php 8.1 and use release of imagick

--- php-fpm/8.1/compile_imagick.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-curl -fsSL https://github.com/Imagick/imagick/archive/$IMAGICK_VERSION.tar.gz | tar xvz --strip 1
-phpize
-./configure
-make
--- php-fpm/8.1/enable_imagick.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-extDir="$(php -d 'display_errors=stderr' -r 'echo ini_get("extension_dir");')"
-
-mv ./imagick.* "$extDir"
-docker-php-ext-enable imagick
